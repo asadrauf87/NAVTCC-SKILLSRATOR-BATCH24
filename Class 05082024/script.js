e8da619aeaed5e287b72acf31471919ae6a430c5
asadrauf87 <asadraufva@gmail.com>
2024-08-08 22:33:00 +0500

    Introduction to JavaScript

---
+++ Class 05082024/script.js
@@ -0,0 +1,179 @@
+console.log("ICE")
+console.warn("ICE")
+console.error("ICE")
+
+// alert("ICE")
+
+//Variables
+
+// var, let, const
+
+// var age = 18;
+// var age = 29; 
+// console.log(age)
+
+// let age = 21;
+// //  age = 20;
+// console.log(age)
+
+// const age = 23;
+// age = 45;
+// console.log(age)
+
+//Hoisting
+// a = 10;
+// console.log(a);
+// var a;
+
+// function getName(){
+//     return "Farzeen"
+// }
+
+// console.log(getName())
+
+// function display(a){
+//     return a;
+// }
+
+// console.log(display(2));
+
+// Arrow Functions
+// const display = a =>  a;
+// console.log(display(2))
+
+// const add = () => {
+//     console.log("Hello")
+// }
+
+// add()
+
+// Immediately Invoked functions
+// (
+//     () => {
+//         console.log("Hello")
+//     }
+// )()
+
+// conditional statements
+
+// if(age == 21){
+//     alert("21")
+// }
+// else if(age == 32){
+//     console.log(32)
+// }
+// else if(age == 30){
+//     console.log(32)
+// }
+// else if(age == 36){
+//     console.log(36)
+// }
+// else if(age == 38){
+//     console.log(32)
+// }
+// else {
+//     alert("No")
+// }
+
+// console.log("Yes")
+let age = 30;
+
+switch(age) {
+    case 20:
+        console.log(20)
+        break;
+    case 30:
+        console.log(30)
+        break;
+    case 40:
+        console.log(40)
+        break;
+    default:
+        console.log("This is Default")
+}
+
+
+
+// let ab = 18;
+
+// function add(){
+//     let ab = 21;
+//     console.log(ab)
+// }
+// add()
+// console.log(ab)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
